2fc993e4e0f56bd45dcf5b27c0ae4d7f06031601
sango735 <735th.floor@gmail.com>
2021-03-02 21:24:56 +0900

    DBについての調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    Item.create(price:2000)
-    Item.create(price:1000)
-    Item.create(price:500)
     @item = Item.all
   end
   def new
