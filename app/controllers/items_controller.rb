2491c893e0a92c9b1ed107fb0d7dcebcba8649c2
sango735 <735th.floor@gmail.com>
2021-03-02 20:07:38 +0900

    レコードの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    Item.create(price: 2000)
+    Item.create(price: 1000)
+    Item.create(price: 500)
     @item = Item.all
-  end
-  def new
-    @item = Item.new
-  end
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to items_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def item_params
-    params.require(:item).permit(:price)
+    
   end
 end
