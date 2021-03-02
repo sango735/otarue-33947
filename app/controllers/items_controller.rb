a15658bc5b94e1d11f44bbae8ae3b812da008b99
sango735 <735th.floor@gmail.com>
2021-03-02 18:31:59 +0900

    データの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def index
     @item = Item.all
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+  private
+  def item_params
+    params.require(:item).permit(:price)
+  end
 end
