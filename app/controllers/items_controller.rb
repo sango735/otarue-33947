7959ea2d4c02c035437a59d720fc8499b009aa39
sango735 <735th.floor@gmail.com>
2021-03-03 15:10:19 +0900

    重複部分の削除や文章・画像の調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,4 @@ class ItemsController < ApplicationController
   def index
     @item = Item.all
   end
-  def new
-    @item = Item.new
-  end
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to items_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def item_params
-    params.require(:item).permit(:price)
-  end
 end
